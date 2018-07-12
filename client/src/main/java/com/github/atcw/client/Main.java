0c70b7aee606afed0aafdf4cbecc27614db6912e
Thorben <thorben.kuck@gmail.com>
2018-07-12 16:52:26 +0200

    main classes introduces

---
+++ client/src/main/java/com/github/atcw/client/Main.java
@@ -0,0 +1,9 @@
+package com.github.atcw.client;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+	}
+
+}
---
+++ server/src/main/java/com/github/atcw/server/Main.java
@@ -0,0 +1,9 @@
+package com.github.atcw.server;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+	}
+
+}
